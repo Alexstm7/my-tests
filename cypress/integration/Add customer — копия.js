453f32effc109f8b52c6b24599d049525844fbd6
Alexstm7 <60259869+Alexstm7@users.noreply.github.com>
2020-02-05 15:14:23 +0200

    cypress

---
+++ cypress/integration/Add customer — копия.js
@@ -0,0 +1,32 @@
+describe('Add part', function() {
+//login
+	it('Add part', function() {
+		//cy.wait (2000)
+		cy.visit('https://us.priority-software.com/demus/DEM4/?_disableplugin=1')
+		//.should('be.visible')
+		//cy.wait (2000)
+		//cy.get('input[class="loginlabel_textboxstyle"]').eq(0)
+		 //.type('QA')
+		 //.should('have.value', 'QA')		 
+		//cy.get('input[class="loginlabel_textboxstyle"]').eq(1)
+		 //.type('123456')
+		 //.should('have.value', '123456')
+
+		
+		
+		 
+
+
+
+
+		 
+
+
+		
+		
+
+
+
+		
+})
+})
